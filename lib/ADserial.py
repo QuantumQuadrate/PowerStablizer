b43463a38a984e7ef8dab2afeb42ccea95ea15b0
xjiang103 <xjiang103@gmail.com>
2017-05-22 15:52:27 -0500

    Create ADserial.py

---
+++ lib/ADserial.py
@@ -0,0 +1,50 @@
+import serial
+
+ADportstr='/dev/ttyACM0'
+
+
+#Prepare the input serial:
+
+ser=serial.Serial()
+ser.baudrate=9600
+ser.port=ADportstr
+ser.open()
+print("Serial information:",ser)
+
+
+
+#Input Control: Some functions related to data reading from the board
+
+#flush(): flush the excess input stored in the serial
+def flush():
+    ser.flushInput()
+    ser.readline()
+    ser.readline()
+    ser.readline()
+    ser.readline()
+    ser.readline()
+    return
+
+#rd(): read a line from the input serial(rd() returns a byte string, need to convert the string to number)
+def rd():
+    return ser.readline()
+
+#seropen():open the serial
+def seropen():
+    return ser.open()
+
+#serclose():close the serial
+def serclose():
+    return ser.close()
+
+#datastoi(x): convert the result fron rd() to a normal number
+def datastoi(x):
+    length=len(x)
+    if length ==5:
+        return 100*(x[0]-48)+10*(x[1]-48)+x[2]-48
+    elif length ==4:
+        return 10*(x[0]-48)+x[1]-48
+    elif length ==3:
+        return x[0]-48
+    elif length ==6:
+        return 1000*(x[0]-48)+100*(x[1]-48)+10*(x[2]-48)+x[3]-48
